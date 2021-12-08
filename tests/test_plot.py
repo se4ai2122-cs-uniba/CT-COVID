326921d4347bd2f7052b3005616d7c23c4216f79
loreloc <lorenzoloconte@outlook.it>
2021-12-08 13:46:56 +0100

    added tests/test_plot.py

---
+++ tests/test_plot.py
@@ -0,0 +1,17 @@
+import torch
+from tempfile import NamedTemporaryFile
+from PIL import Image
+from covidx.utils.plot import save_attention_map
+
+
+def test_save_attention_map():
+    padding = 4
+    height, width = 224, 224
+    img = torch.rand(1, 1, height, width)
+    att1 = torch.rand(1, 1, height // 16, width // 16)
+    att2 = torch.rand(1, 1, height // 32, width // 32)
+    fp = NamedTemporaryFile('r+b', suffix='.png')
+    save_attention_map(fp.name, img, att1, att2)
+    with Image.open(fp) as img:
+        assert len(img.getbands()), 3
+        assert img.size == (width * 3 + padding * 4, height + padding * 2)
