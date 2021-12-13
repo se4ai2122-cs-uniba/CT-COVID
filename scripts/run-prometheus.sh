b754809cd9f46ac6404fb6e60895e9b720087658
loreloc <lorenzoloconte@outlook.it>
2021-12-13 17:50:56 +0100

    added --network=host in run-prometheus.sh

--- scripts/run-prometheus.sh
+++ scripts/run-prometheus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 9090:9090 -v "$PWD/prometheus.yml":/etc/prometheus/prometheus.yml prom/prometheus
+docker run --network=host -p 9090:9090 -v "$PWD/prometheus.yml":/etc/prometheus/prometheus.yml prom/prometheus
