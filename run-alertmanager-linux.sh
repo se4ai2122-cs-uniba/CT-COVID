bbc16ba0480268ae09e6e27e36c0967cce0e80ca
Francesco Benedetti <45521145+itsfrank98@users.noreply.github.com>
2021-12-29 14:17:28 +0100

    Delete run-alertmanager-linux.sh
    Useless file

--- run-alertmanager-linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ..
-docker run -d -p 9093:9093 --add-host host.docker.internal:host-gateway --name alertmanager -v "$PWD/alert_rules.yml":/etc/prometheus/alert_rules.yml prom/alertmanager
