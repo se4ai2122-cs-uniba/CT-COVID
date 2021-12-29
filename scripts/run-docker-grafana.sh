df0281ecb5275e228dbc211c122099e879dc2d80
loreloc <lorenzoloconte@outlook.it>
2021-12-29 17:20:39 +0100

    removed scripts for prometheus and grafana

--- scripts/run-docker-grafana.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run -d -p 3000:3000 --add-host host.docker.internal:host-gateway \
-    --name=grafana grafana/grafana-enterprise
--- scripts/run-docker-prometheus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run -d -p 9090:9090 --add-host host.docker.internal:host-gateway \
-    -v "$PWD/prometheus.yml":/etc/prometheus/prometheus.yml \
-    --name=prometheus prom/prometheus
